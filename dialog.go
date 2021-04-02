6ad561c3c64b84a44dd40a60e9b3f2ac2f15996c
Aleksandr Folomkin <bambruysk@gmail.com>
2021-04-02 22:08:02 +0300

    work in progress

--- dialog.go
+++ dialog.go
@@ -1,19 +1,213 @@
 package main
 
-type DialogState struct { 
-	state int 
-}
+import (
+	"errors"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+type DialogState DialogNode
+
+/*
 const (
-	// userState 
-	IDLE int=  iota
-	UserSettings 
-	Menu 
+	// userState
+	IDLE DialogState = iota
+	UserSettings
+	MainMenu
 	HoldeCalc
 )
 
-
-type User  struct {
-	State  DialogState 
+*/
+// Пользователь
+type User struct {
+	State DialogState
 }
 
-type Users map [string] User 
+func (u *User) SetState(state DialogState) {
+	u.State = state
+}
+
+func NewUser() User {
+	return User{
+		State: CreateDeaultState(),
+	}
+}
+
+
+
+
+/// Хранилище пользователей, потом заменим на BD
+
+type UserID int
+
+type UserStorager interface {
+	// Get user
+	Get(id UserID) (User, error)
+	// Geto or create user w default state
+	GetOrCreate(id UserID) (User, bool)
+
+	Update(id UserID, user User) error
+
+	Create(id UserID, user User)
+}
+
+type Users map[UserID]User
+
+func NewUsers() Users {
+	users := make(Users)
+	return users
+}
+
+func (u Users) Get(id UserID) (User, error) {
+	user, exist := u[id]
+	if !exist {
+		return User{}, errors.New("User not found")
+	}
+	return user, nil
+}
+
+func (u Users) GetOrCreate(id UserID) (User, bool) {
+	user, exist := u[id]
+	if !exist {
+		user = NewUser()
+		u.Create(id, user)
+		return user, true
+	}
+	return user, false
+}
+
+func (u Users) Create(id UserID, user User) {
+	u[id] = user
+}
+
+func (u Users) Update(id UserID, user User) error {
+	_, exist := u[id]
+	if !exist {
+		return errors.New("User not found ")
+	}
+	u[id] = user
+	return nil
+}
+
+type DialogContent struct {
+	Message string
+	Media   interface{}
+}
+
+type Handler func(interface{})
+
+// Каждая нода диалога является состоянием пользвателя
+type DialogNode struct {
+	// Текст ии дугое сообщение оторжаео при переходе на данный узел
+	Content DialogContent
+	// Клавиатура
+	Keyboard *tb.ReplyMarkup
+	// Принимает текст в качетсве
+	IsReceiveText bool
+	//
+	HasKeyboard bool
+}
+
+
+
+func NewDialogNode(content DialogContent, buttons ...tb.Btn) DialogNode {
+	keyboard := &tb.ReplyMarkup{ResizeReplyKeyboard: true}
+
+	// для простоты будет выстраовиать все кнопки в столбик, то есть каждая кнопка
+	rows := make([]tb.Row, len(buttons))
+	for i, btn := range buttons {
+		rows[i] = keyboard.Row(btn)
+	}
+	keyboard.Reply(rows...)
+
+	return DialogNode{
+		Content:  content,
+		Keyboard: keyboard,
+	}
+}
+/// Implement sendable inteface of telebot
+func (dn DialogNode)  Send(*tg.Bot, tg.Recipient, *tg.SendOptions) (*tg.Message, error) {
+	// not implemented yet
+
+	return &tg.Message{}, errors.New("Not implemented yet")
+
+}
+
+
+func (dn DialogNode) GetHandler () interface{} {
+	return func 
+
+
+}
+
+// Next compute next DialogNode from update for user
+func (dn *DialogNode) Next(update interface{}) DialogNode {
+	// switch tu:= update.(type) {
+	// case *tb.Message: {
+
+	// 	tu.
+	// }
+	// }
+	return DialogNode{}
+}
+
+// my dialog buttons
+
+var (
+	btnSettings   = (&tb.ReplyMarkup{ResizeReplyKeyboard: true}).Text("Настройки")
+	btnCalculator = (&tb.ReplyMarkup{ResizeReplyKeyboard: true}).Text("Поместья")
+	btnMainMenu   = (&tb.ReplyMarkup{ResizeReplyKeyboard: true}).Text("Главное меню")
+)
+
+var users UserStorager
+
+func CreateDialog(bot *tb.Bot, enterPoint interface{}) {
+
+	start_dn := NewDialogNode(DialogContent{
+		Message: "Добро пожаловать",
+		Media:   nil,
+	}, btnSettings, btnCalculator)
+
+	start_dn.HasKeyboard = true
+
+	settings_dn := NewDialogNode(DialogContent{
+		Message: "Выберите настройку",
+		Media:   nil,
+	}, btnSettings, btnCalculator, btnMainMenu)
+
+	// Add starting point
+	*bot.Handle(enterPoint, func(m *tb.Message) {
+		
+		user_id := UserID(m.Sender.ID)
+
+		user, created := users.GetOrCreate(user_id)
+
+		*bot.Send(m.Sender, start_dn.DialogContent.Message, start_dn.Keyboard)
+	} )
+
+	// Setiings
+	
+	*bot.Handle(&btnSettings, func(m *tb.Message) {
+		
+		id := UserID(m.Sender.ID)
+		user, err := users.Get(id)
+		if 
+		user.SetState(settings_dn)
+		users.Update(id, user)
+
+		
+
+		*bot.Send(m.Sender, start_dn.DialogContent.Message, start_dn.Keyboard)
+	} )
+
+
+
+}
+
+func CreateDeaultState() DialogState {
+	return DialogState(NewDialogNode(DialogContent{
+		Message: "Добро пожаловать",
+		Media:   nil,
+	}, btnSettings, btnCalculator))
+
+}
--- main.go
+++ main.go
@@ -1,16 +1,12 @@
-
 package main
 
 import (
+	"fmt"
 	"log"
 	"time"
 
-
 	tb "gopkg.in/tucnak/telebot.v2"
 )
-
-
-
 
 func main() {
 	b, err := tb.NewBot(tb.Settings{
@@ -29,17 +25,143 @@ func main() {
 
 	log.Println("Bot started")
 
-	b.Handle("/hello", func(m *tb.Message) {
-		log.Println("Message", m.Sender.ID, m.Sender.FirstName, m.Sender.LastName )
-		b.Send(m.Sender, "Hello World!")
+	// b.Handle("/hello", func(m *tb.Message) {
+	// 	log.Println("Message", m.Sender.ID, m.Sender.FirstName, m.Sender.LastName)
+	// 	b.Send(m.Sender, "Hello World!")
+	// })
+
+	// b.Handle(tb.OnText, func(m *tb.Message) {
+
+	// 	log.Println("User", m.Sender.ID, m.Sender.FirstName, m.Sender.LastName)
+	// 	log.Println("Message", m.Text)
+	// 	b.Send(m.Sender, "Hello World!")
+	// })
+
+	// b.Start()
+
+	var (
+
+		// Окно приветсвтия - регистрация  -
+
+		// кнопка вызова главного меню
+		btnMainMenu = (&tb.ReplyMarkup{ResizeReplyKeyboard: true}).Text("В главное меню")
+		// Главное меню - Настройки | Считать
+		btnSettings   = (&tb.ReplyMarkup{ResizeReplyKeyboard: true}).Text("Настройки")
+		btnCalculator = (&tb.ReplyMarkup{ResizeReplyKeyboard: true}).Text("Поместья")
+
+		// Настройки - Oпция 1 | Oпция 2 | Oпция 3
+		// Считать  -  Имя игрока - Добавить поместья - Снять кэш - Улучшить поместье.
+		//                             |      |
+		//								> ----^
+
+		// Universal markup builders.
+		menu     = &tb.ReplyMarkup{ResizeReplyKeyboard: true}
+		selector = &tb.ReplyMarkup{}
+
+		// Reply buttons.
+		btnHelp = menu.Text("ℹ Help")
+		//	btnSettings = menu.Text("⚙ Settings")
+
+		// Inline buttons.
+		//
+		// Pressing it will cause the client to
+		// send the bot a callback.
+		//
+		// Make sure Unique stays unique as per button kind,
+		// as it has to be for callback routing to work.
+		//
+		btnPrev = selector.Data("⬅", "prev")
+		btnNext = selector.Data("➡", "next")
+	)
+
+	var users UserStorager
+	users = NewUsers()
+
+	// menu.Reply(
+	// 	menu.Row(btnHelp),
+	// 	menu.Row(btnSettings),
+	// )
+	// selector.Inline(
+	// 	selector.Row(btnPrev, btnNext),
+	// )
+
+	UpdateUserState := func(id string, state DialogState) {
+		user, _ := users.Get(id)
+		user.SetState(MainMenu)
+		users.Update(id, user)
+	}
+
+	mainMenu := DialogNode{
+		Content: DialogContent{
+			Message: "Выберите, что бы вы хотели сделать",
+			Media:   nil,
+		},
+		Keyboard: &tb.ReplyMarkup{},
+	}
+	mainMenu.Keyboard.Reply(
+		mainMenu.Keyboard.Row(btnCalculator),
+		mainMenu.Keyboard.Row(btnSettings),
+	)
+
+	// Command: /start <PAYLOAD>
+	b.Handle("/start", func(m *tb.Message) {
+		if !m.Private() {
+			return
+		}
+		user, created := users.GetOrCreate(id)
+		user.SetState(MainMenu)
+		users.Update(id, user)
+
+		//  Для нового пользовтеля отсылаемприветсвенное соообщение
+		if created {
+			//
+			b.Send(m.Sender, fmt.Sprintf("Рад с тобой познаокмиться %s. Я бот и я буду тебе помогать в экономике", m.Sender.FirstName))
+		} else {
+			b.Send(m.Sender, fmt.Sprintf("с возвращением %s", m.Sender.FirstName))
+		}
+		b.Send(m.Sender, "Начнем", mainMenu)
 	})
 
-	b.Handle(tb.OnText, func(m *tb.Message) {
+	// On reply button pressed (message)
+	b.Handle(&btnHelp, func(m *tb.Message) {
+		log.Println("User", m.Sender.ID, m.Sender.FirstName, m.Sender.LastName)
+		log.Println("Message", m.Text)
+		b.Send(m.Sender, "Hello World!", selector)
+	})
 
-		log.Println("User", m.Sender.ID, m.Sender.FirstName, m.Sender.LastName )
-		log.Println("Message", m.Text )
-		b.Send(m.Sender, "Hello World!")
+	b.Handle(&btnCalculator, func(m *tb.Message) {
+		UpdateUserState(string(m.Sender.ID), HoldeCalc)
+		b.Send(m.Sender, "Сообщи, пожалуйста, мне имя игрока")
+	})
+
+	// Send hand
+
+	// On inline button pressed (callback)
+	b.Handle(&btnPrev, func(c *tb.Callback) {
+		// ...
+		// Always respond!
+		b.Respond(c, &tb.CallbackResponse{
+			Text: c.Message.Text,
+		})
+	})
+
+	type DialogNode struct {
+		// Текст ии дугое сообщение оторжаео при переходе на данный узел
+		Content DialogContent
+		// Клавиатура
+		Keyboard *tb.ReplyMarkup
+		//
+	}
+
+	// On reply button pressed (message)
+	b.Handle(&btnMainMenu, func(m *tb.Message) {
+
+		log.Println("main menu", m.Text)
+
+		b.Send(m.Sender, "Выберите, что бы вы хотели сделать", mainMenu)
+
 	})
 
 	b.Start()
+
 }
