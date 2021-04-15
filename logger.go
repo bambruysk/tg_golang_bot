9976dc060e24275fc5a48b0906483b6bb331ad84
bambruysk <bambruysk@gmail.com>
2021-04-15 06:16:51 +0000

    цщз

--- logger.go
+++ logger.go
@@ -9,20 +9,29 @@ type BotLog struct {
 	Bot   *tb.Bot
 }
 
+//NewBotLog create ne l0gger instanse with deafult subscribers 
 func NewBotLog(bot *tb.Bot) BotLog {
 	chats := make([]tb.ChatID, 1)
-	chats[0] = tb.ChatID(342270809)
-
+	chats[0] = tb.ChatID(-1001241689222)
 	return BotLog{
 		Chats: chats,
 		Bot:   bot,
 	}
 }
 
-func (b *BotLog) AddSubscriber(id tb.ChatID) {
+// AddSubscriber return true if subscriber added
+func (b *BotLog) AddSubscriber(id tb.ChatID) bool {
+	// repalce to index& 
+	for _,c :=  range b.Chats {
+		if c ==  id {
+			return false
+		}
+	}
 	b.Chats = append(b.Chats, id)
+	return true
 }
 
+//Log send message to subscribers
 func (b *BotLog) Log(message string) {
 	for _, chat := range b.Chats {
 		b.Bot.Send(chat, message)
